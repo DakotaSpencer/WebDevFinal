3eac3fee463d767af3df94feadba8b3c042570fd
Brenden Corrington-Hubbard <80130428+bcorringtonhubbard@users.noreply.github.com>
2021-05-26 10:53:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const dbPath = "mongodb+srv://JWilliams1233:9542715.Jw1233@cluster0.ujp1m.mongodb.net/Users?retryWrites=true&w=majority"
 
+const express = require('express');
+const pug = require('pug');
 const mongoose = require('mongoose');
+const { allowedNodeEnvironmentFlags } = require('node:process');
 mongoose.Promise = global.Promise;
+
+const app = express();
 
 mongoose.connect(dbPath, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
+
+/* app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/public')));*/
 
 let mdb = mongoose.connection;
 mdb.on('error', console.error.bind(console, 'connection error'));
