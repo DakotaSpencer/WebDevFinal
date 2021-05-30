1f1f921aed120e21931d361f6f3e6031773b6f8c
Brenden Corrington-Hubbard <80130428+bcorringtonhubbard@users.noreply.github.com>
2021-05-29 18:29:44 -0600

    idk lol

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(cors())
+app.use(cors());
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
