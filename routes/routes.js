c15865a9aa8159a538b301950f6f59bae1b8fbcb
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2021-05-31 17:11:29 -0600

    Revert "fuck"
    This reverts commit f65c4251b1d77ed27c6255a2dc4bd8498597710b.

--- routes/routes.js
+++ routes/routes.js
@@ -28,9 +28,7 @@ let User = mongoose.model('User_Collection', userSchema);
 exports.login = (req,res) => {
     res.render('login', {
       title: 'Login',
-      config: config,
-      cookie: lastVisited,
-      user: req.session.user
+      config: config
       //the first page that appears
     });
     //res.redirect('/home')
@@ -39,53 +37,26 @@ exports.login = (req,res) => {
 exports.home = (req, res) => {
     res.render('home', {
       title: 'Home',
-      config: config,
-      cookie: lastVisited,
-      user: req.session.user
+      config: config
     })
 };
 
 exports.createAccount = (req, res) => {
     res.render('createAccount', {
       title: 'Create an Account',
-      config: config,
-      cookie: lastVisited,
-      user: req.session.user
+      config: config
     })
     //res.redirect('/createAccount')
 };
 
 exports.makeHash = (req,res) => {
-  if(req.body.password && req.body.username && req.body.email && req.body.age){
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(req.body.password, salt, (err, myHash) => {
-          let user = new User({
-            username: req.body.username,
-            password: myHash,
-            email: req.body.email,
-            age: req.body.age,
-            questionOne: req.body.animal,
-            questionTwo: req.body.color,
-            questionThree: req.body.food,  
-          });
-          user.save((err, para2) => {
-            //returns err if theres an error
-            if(err) {
-              console.error(err);
-              let lastVisited = req.cookies.createAccountLastVisited;
-              res.cookie("createAccountLastVisited", `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`)
-              
-              res.render('createAccount', {
-                errorMessage: "Creating Account Failed",
-                config: config,
-                cookie: lastVisited,
-                user: req.session.user
-              });
-            } else {
-              res.redirect('/')
-            };
-            console.log(req.body.username + ' added.');
-          });
+  bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(req.body.password, salt, (err, myHash) => {
+        let user = new User({
+          username: req.body.username,
+          password: myHash,
+          email: req.body.email,
+          age: req.body.age
         });
         user.save((err, para2) => {
           //returns err if theres an error
@@ -100,59 +71,11 @@ exports.makeHash = (req,res) => {
           console.log(req.body.username + ' added.');
         });
       });
-  };
+  });
 };
 //Checking for user Authenitcation
 exports.loginAuth = (req,res) => {
-  if(req.body && req.body.password && req.body.username){
-    User.find({username: req.body.username}, (err,user) => {
-      if (user && user[0]){
-        bcrypt.compare(req.body.password, user[0].password, (err, res1) => {
-          if(res1 === true){
-            req.session.user = user[0];
-            let lastVisited = req.cookies.homeLastVisited;
-            res.cookie("homeLastVisited", `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`)
-            res.redirect("/home")
-          } else {
-            //res.render("login", {title: "login page", config: config})
-            res.redirect('/');
-          }
-        });
-      }else{
-        res.redirect('/')
-      }
-    });
-  } else {
-    res.redirect('/');
-  }
-};
-
-exports.editUser = (req, res) => {
-  let lastVisited = req.cookies.editLastVisited;
-  res.cookie("editLastVisited", `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`)
-  res.render('editPerson', {
-    title: "Edit User",
-    config: config,
-    cookie: lastVisited,
-    user: req.session.user
-  })
-}
-
-exports.editPerson = (req, res) => {
-  //Reads frin database
-  User.findById(req.body.id, (err, user) => {
-    if(err) return console.error(err);
-    user.username= req.body.username;
-    //user.password= myHash,
-    user.email= req.body.email;
-    user.age= req.body.age;
-    user.questionOne= req.body.animal;
-    user.questionTwo= req.body.color;
-    user.questionThree= req.body.food;
-    user.save((err, user) => {
-      if(err) return console.error(err);
-      console.log(req.body.name + ' updated.');
-    });
-    res.redirect('/');
-  })
-};
+    User.findById(req.params.id, (err,user) => {
+      bcrypt.compare(req.params.password)
+    })
+ };
